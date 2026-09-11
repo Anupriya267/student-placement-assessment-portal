316536d1bf9eeda6b933ffa80c159e6fbdfef254
Anupriya267 <166500195+Anupriya267@users.noreply.github.com>
2026-09-12 01:25:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,8 @@
 
 function login(){
 
-let username = document.getElementById("username").value;
-let password = document.getElementById("password").value;
+let username = document.getElementById("username").value.trim();
+let password = document.getElementById("password").value.trim();
 
 
 if(username === "admin" && password === "admin123"){
@@ -27,19 +27,31 @@ alert("Invalid Username or Password");
 }
 
 
+
 // TIMER
 
 if(document.getElementById("timer")){
 
 let time = 40 * 60;
 
+
 let timer = setInterval(function(){
 
+
+let timerBox = document.getElementById("timer");
+
+
+if(timerBox){
+
 let min = Math.floor(time / 60);
+
 let sec = time % 60;
 
-document.getElementById("timer").innerHTML =
+
+timerBox.innerHTML =
 min + ":" + (sec < 10 ? "0" : "") + sec;
+
+}
 
 
 time--;
@@ -48,21 +60,27 @@ time--;
 if(time < 0){
 
 clearInterval(timer);
+
 submitTest();
 
 }
+
 
 },1000);
 
 }
 
 
-// SUBMIT
+
+// SUBMIT ASSESSMENT
 
 function submitTest(){
 
 let score = 0;
 
+
+
+// MCQ ANSWERS
 
 let answers = {
 
@@ -80,15 +98,18 @@ q10:"final"
 };
 
 
+
+// Check MCQ
+
 for(let q in answers){
 
-let answer =
+let selected =
 document.querySelector(
 'input[name="'+q+'"]:checked'
 );
 
 
-if(answer && answer.value == answers[q]){
+if(selected && selected.value === answers[q]){
 
 score++;
 
@@ -97,9 +118,48 @@ score++;
 }
 
 
+
+// Error correction
+
+let error1 =
+document.querySelector(
+'input[name="error1"]:checked'
+);
+
+
+if(error1 && error1.value === "semicolon"){
+
+score++;
+
+}
+
+
+
+let error2 =
+document.querySelector(
+'input[name="error2"]:checked'
+);
+
+
+if(error2 && error2.value === "addsemicolon"){
+
+score++;
+
+}
+
+
+
+// Save result
+
 localStorage.setItem("score",score);
 
+localStorage.setItem("total",12);
+
+
+
+// Go result page
 
 window.location.href="results.html";
+
 
 }
