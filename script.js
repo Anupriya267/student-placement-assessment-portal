5ce3f4ae54098f665f3794af6765df4b84f7c161
Anupriya267 <166500195+Anupriya267@users.noreply.github.com>
2026-09-12 00:18:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,174 +1,23 @@
-// =======================
-// LOGIN FUNCTION
-// =======================
+function login(){
 
-function login() {
+let username = document.getElementById("username").value;
 
-    let username = document.getElementById("username").value;
-    let password = document.getElementById("password").value;
+let password = document.getElementById("password").value;
 
 
-    if (username === "admin" && password === "admin123") {
+alert(username + " " + password);
 
-        window.location.href = "admin.html";
 
-    }
+if(username=="admin" && password=="admin123"){
 
-    else if (username === "student1" && password === "1234") {
-
-        window.location.href = "student.html";
-
-    }
-
-    else {
-
-        alert("Invalid Username or Password");
-
-    }
+window.location.href="admin.html";
 
 }
 
+else{
 
-
-// =======================
-// TIMER
-// =======================
-
-if (document.getElementById("timer")) {
-
-    let time = 40 * 60;
-
-
-    let timer = setInterval(function () {
-
-
-        let minutes = Math.floor(time / 60);
-
-        let seconds = time % 60;
-
-
-        document.getElementById("timer").innerHTML =
-            minutes + ":" + (seconds < 10 ? "0" : "") + seconds;
-
-
-        time--;
-
-
-        if (time < 0) {
-
-            clearInterval(timer);
-
-            alert("Time Over");
-
-            submitTest();
-
-        }
-
-
-    }, 1000);
+alert("Wrong login");
 
 }
-
-
-
-// =======================
-// SUBMIT ASSESSMENT
-// =======================
-
-function submitTest() {
-
-
-    let score = 0;
-
-
-    let answers = {
-
-        q1: "new",
-        q2: "for",
-        q3: "int",
-        q4: "==",
-        q5: "if",
-        q6: "0",
-        q7: "Object Oriented",
-        q8: "&&",
-        q9: "main",
-        q10: "final"
-
-    };
-
-
-
-    for (let q in answers) {
-
-
-        let answer =
-        document.querySelector(
-        'input[name="' + q + '"]:checked'
-        );
-
-
-        if (answer && answer.value === answers[q]) {
-
-            score++;
-
-        }
-
-    }
-
-
-
-    // Error correction
-
-    let error1 =
-    document.querySelector(
-    'input[name="error1"]:checked'
-    );
-
-
-    if (error1 && error1.value === "semicolon") {
-
-        score += 2;
-
-    }
-
-
-
-    let error2 =
-    document.querySelector(
-    'input[name="error2"]:checked'
-    );
-
-
-    if (error2 && error2.value === "addsemicolon") {
-
-        score += 2;
-
-    }
-
-
-
-    // Coding marks
-
-    let codes =
-    document.querySelectorAll("textarea");
-
-
-    codes.forEach(function(code){
-
-        if(code.value.trim() !== "") {
-
-            score += 3;
-
-        }
-
-    });
-
-
-
-    localStorage.setItem("score", score);
-
-
-    window.location.href = "results.html";
-
 
 }
