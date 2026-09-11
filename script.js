cd830d1c4fbd715a9a2d065442cc2666d598e129
Anupriya267 <166500195+Anupriya267@users.noreply.github.com>
2026-09-12 00:59:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,58 +1,117 @@
-alert("Script Loaded");
+// LOGIN FUNCTION
 
+function login(){
+
+    let username = document.getElementById("username").value.trim();
+    let password = document.getElementById("password").value.trim();
+
+
+    if(username === "admin" && password === "admin123"){
+
+        window.location.href = "admin.html";
+
+    }
+
+    else if(username === "student1" && password === "1234"){
+
+        window.location.href = "student.html";
+
+    }
+
+    else{
+
+        alert("Invalid Username or Password");
+
+    }
+
+}
+
+
+
+// TIMER FOR ASSESSMENT PAGE
 
 let time = 40 * 60;
 
 let timer = setInterval(function(){
 
-let timerBox = document.getElementById("timer");
+    let timerBox = document.getElementById("timer");
 
-if(timerBox){
+    if(timerBox){
 
-let min = Math.floor(time / 60);
-let sec = time % 60;
+        let minutes = Math.floor(time / 60);
+        let seconds = time % 60;
 
-timerBox.innerHTML = min + ":" + (sec < 10 ? "0" : "") + sec;
+        timerBox.innerHTML =
+        minutes + ":" + (seconds < 10 ? "0" : "") + seconds;
 
-}
+        time--;
 
-time--;
+    }
+
+
+    if(time < 0){
+
+        clearInterval(timer);
+
+        localStorage.setItem("score",0);
+
+        window.location.href = "results.html";
+
+    }
+
 
 },1000);
 
 
 
+
+// SUBMIT ASSESSMENT
+
 function submitTest(){
 
-window.location.href="results.html";
-
-}
+    let score = 0;
 
 
+    let answers = {
 
-function login(){
+        q1:"new",
+        q2:"for",
+        q3:"int",
+        q4:"==",
+        q5:"if",
+        q6:"0",
+        q7:"Object Oriented",
+        q8:"&&",
+        q9:"main",
+        q10:"final",
+        error1:"semicolon",
+        error2:"addsemicolon"
 
-let username = document.getElementById("username").value.trim();
-
-let password = document.getElementById("password").value.trim();
+    };
 
 
-if(username=="admin" && password=="admin123"){
+    for(let question in answers){
 
-window.location.href="admin.html";
+        let selected =
+        document.querySelector(
+        'input[name="'+question+'"]:checked'
+        );
 
-}
 
-else if(username=="student1" && password=="1234"){
+        if(selected && selected.value === answers[question]){
 
-window.location.href="student.html";
+            score++;
 
-}
+        }
 
-else{
+    }
 
-alert("Invalid Username or Password");
 
-}
+    localStorage.setItem("score", score);
+
+    localStorage.setItem("total", "12");
+
+
+    window.location.href = "results.html";
 
 }
