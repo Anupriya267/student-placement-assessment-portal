d8b81d7ecef23acdf98f306b5755b8382d4e480c
Anupriya267 <166500195+Anupriya267@users.noreply.github.com>
2026-09-12 00:57:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -28,21 +28,24 @@ window.location.href="results.html";
 
 }
 
+
+
 function login(){
 
 let username = document.getElementById("username").value.trim();
+
 let password = document.getElementById("password").value.trim();
 
 
-if(username === "admin" && password === "admin123"){
+if(username=="admin" && password=="admin123"){
 
-window.location.href = "admin.html";
+window.location.href="admin.html";
 
 }
 
-else if(username === "student1" && password === "1234"){
+else if(username=="student1" && password=="1234"){
 
-window.location.href = "student.html";
+window.location.href="student.html";
 
 }
 
@@ -51,131 +54,5 @@ else{
 alert("Invalid Username or Password");
 
 }
-
-}
-
-
-
-// TIMER
-
-let time = 40 * 60;
-
-let timer = setInterval(function(){
-
-let minutes = Math.floor(time / 60);
-
-let seconds = time % 60;
-
-
-if(document.getElementById("timer")){
-
-document.getElementById("timer").innerHTML =
-minutes + ":" + (seconds < 10 ? "0" : "") + seconds;
-
-}
-
-
-time--;
-
-
-if(time < 0){
-
-clearInterval(timer);
-
-alert("Time Over! Test Submitted");
-
-window.location.href="results.html";
-
-}
-
-
-},1000);
-
-
-
-
-// SUBMIT TEST
-
-function submitTest(){
-
-let score = 0;
-
-
-// MCQ ANSWERS
-
-let answers = {
-
-q1:"new",
-q2:"for",
-q3:"int",
-q4:"==",
-q5:"if",
-q6:"0",
-q7:"Object Oriented",
-q8:"&&",
-q9:"main",
-q10:"final"
-
-};
-
-
-// CHECK MCQ
-
-for(let q in answers){
-
-let selected = document.querySelector(
-'input[name="'+q+'"]:checked'
-);
-
-
-if(selected && selected.value === answers[q]){
-
-score++;
-
-}
-
-}
-
-
-
-// ERROR QUESTIONS
-
-let errorAnswers = {
-
-error1:"semicolon",
-
-error2:"addsemicolon"
-
-};
-
-
-for(let q in errorAnswers){
-
-let selected = document.querySelector(
-'input[name="'+q+'"]:checked'
-);
-
-
-if(selected && selected.value === errorAnswers[q]){
-
-score++;
-
-}
-
-}
-
-
-
-// SAVE RESULT
-
-localStorage.setItem("score", score);
-
-localStorage.setItem("total", "12");
-
-
-// GO TO RESULT PAGE
-
-window.location.href = "results.html";
-
 
 }
